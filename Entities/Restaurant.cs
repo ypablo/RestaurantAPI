a6defb572890a145bc9efa94b231967fdf1b9dbf
ypablo <yfedora@gmail.com>
2022-11-16 21:35:02 +0000

    Add project files.

---
+++ Entities/Restaurant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Entities
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Descrition { get; set; }
+
+        public string Category { get; set; }
+
+        public bool HasDelivery { get; set; }
+
+        public string ContactEmail { get; set; }
+
+        public string ContactNumber { get; set; }
+        public int AdressId { get; set; }
+
+        public virtual Address Address { get; set; }
+
+        public virtual List<Dish> Dishes { get; set; }
+
+    }
+}
---
+++ Entities/RestaurantDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+using System.Data;
+
+namespace RestaurantAPI.Entities
+{
+    public class RestaurantDbContext: DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+
+        public DbSet<Address> Addresses { get; set; }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Restaurant>()
+            .Property(r => r.Name)
+            .IsRequired()
+            .HasMaxLength(25);
+        modelBuilder.Entity<Dish>()
+            .Property(d => d.Name)
+            .IsRequired();
+    }
+
+
+}
